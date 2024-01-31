e3d6962a8ff0b7a36c7716eefe5a67d2ea6a8c65
Noel Binns <noelbinns@noels-mbp.misc.iastate.edu>
2024-01-31 13:31:40 -0600

    Test code with working button

--- Experiments/Noel_Binns/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/Noel_Binns/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -22,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         messageText.setText("Hello World");
+        Button simpleButton = (Button) findViewById(R.id.simpleButton);
+        simpleButton.setBackgroundColor(Color.BLACK);//set the black color of button background
+        simpleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "This Button has worked", Toast.LENGTH_LONG).show();//display the text of button1
+            }
+        });
     }
 }
