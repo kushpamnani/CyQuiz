9dc5d679886dc00e881e855263439b0677844c6b
Noel Binns <noelbinns@noels-mbp.misc.iastate.edu>
2024-01-30 18:13:48 -0600

    test push Noel Binns

---
+++ Experiments/Noel_Binns/app/src/main/java/com/example/noel_android_hello/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.noel_android_hello
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.noel_android_hello.ui.theme.Noel_Android_HelloTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Noel_Android_HelloTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Noel_Android_HelloTheme {
+        Greeting("Android")
+    }
+}
