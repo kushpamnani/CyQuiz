5e69840123db2c82758759b6ec0977c7623f7546
Dalton Clark <dbclark@iastate.edu>
2024-02-03 17:29:13 -0600

    exp 2 and 3

---
+++ Experiments/Dalton_Clark/exp3/src/main/java/coms309/dog/Dog.java
@@ -0,0 +1,56 @@
+package coms309.dog;
+
+public class Dog {
+
+    private String name;
+
+    protected String owner;
+
+    private String breed;
+
+    private String favTreat;
+
+    public Dog(){
+
+    }
+
+    public Dog(String name, String owner, String breed, String favTreat){
+        this.name = name;
+        this.owner = owner;
+        this.breed = breed;
+        this.favTreat = favTreat;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return this.breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getFavTreat() {
+        return this.favTreat;
+    }
+
+    public void setFavTreat(String favTreat) {
+        this.favTreat = favTreat;
+    }
+
+    @Override
+    public String toString() {
+        return name + " "
+                + breed + " "
+                + favTreat;
+    }
+}
+
