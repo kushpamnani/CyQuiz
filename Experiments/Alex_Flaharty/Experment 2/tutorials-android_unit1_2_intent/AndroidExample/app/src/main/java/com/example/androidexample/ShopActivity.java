f0171e0de215f1a8e6d95d30c71afb2eee27a418
Alex_Flaharty <alex25@iastate.edu>
2024-02-03 21:26:51 -0600

    Finished shop

--- Experiments/Alex_Flaharty/Experment 2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/ShopActivity.java
+++ Experiments/Alex_Flaharty/Experment 2/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/ShopActivity.java
@@ -10,18 +10,44 @@ public class ShopActivity extends AppCompatActivity {
     private Button More;
     private Button less;
     private int Level;
-    private TextView Cost;
+    private TextView Cost_txt;
+    int Cost;
+    int Final_Cost;
     private TextView Skill;
-    private TextView Final_Cost;
+    private TextView Final_Cost_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-
+        Level = 0;
+        Cost = 100;
         More = findViewById(R.id.More);
         less = findViewById(R.id.Less);
-        Cost = findViewById(R.id.Normal_Cost_txt);
-        Final_Cost = findViewById(R.id.Final_Cost);
-        Skill = findViewById(R.id.Skill_level);
+        Cost_txt = findViewById(R.id.Cost);
+        Final_Cost_txt = findViewById(R.id.Final_Cost);
+        Skill = findViewById(R.id.Skill_Level);
+        Cost_txt.setText(String.valueOf(Cost));
+        Skill.setText("1");
+        More.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Level++;
+                Skill.setText(String.valueOf(Level));
+                Final_Cost = Cost -(5*Level);
+                Final_Cost_txt.setText(String.valueOf(Final_Cost));
+            }
+        });
+        less.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Level--;
+                Skill.setText(String.valueOf(Level));
+                Final_Cost = Cost -(5*Level);
+                Final_Cost_txt.setText(String.valueOf(Final_Cost));
+            }
+        });
+        Final_Cost = Cost -(5*Level);
+        Final_Cost_txt.setText(String.valueOf(Final_Cost));
+
     }
 }
