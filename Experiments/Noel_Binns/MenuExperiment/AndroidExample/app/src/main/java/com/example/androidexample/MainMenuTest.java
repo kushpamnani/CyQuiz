900f741eccdf0b0fe9c8c82b2ffab861eca80a8f
Noel Binns <noelbinns@noels-mbp.misc.iastate.edu>
2024-02-16 23:01:18 -0600

    main menu rough concept

---
+++ Experiments/Noel_Binns/MenuExperiment/AndroidExample/app/src/main/java/com/example/androidexample/MainMenuTest.java
@@ -0,0 +1,30 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainMenuTest extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_test);             // link to Main activity XML
+
+
+
+
+
+
+
+
+
+    }
+}
