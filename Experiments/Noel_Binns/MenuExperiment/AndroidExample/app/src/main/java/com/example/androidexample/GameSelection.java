22159b25e110baf15a46777dbc1d885e57c3b3c5
Noel Binns <noelbinns@noels-mbp.misc.iastate.edu>
2024-02-20 11:28:31 -0600

    added small back button

--- Experiments/Noel_Binns/MenuExperiment/AndroidExample/app/src/main/java/com/example/androidexample/GameSelection.java
+++ Experiments/Noel_Binns/MenuExperiment/AndroidExample/app/src/main/java/com/example/androidexample/GameSelection.java
@@ -1,15 +1,28 @@
 package com.example.androidexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GameSelection extends AppCompatActivity {
 
+public class GameSelection extends AppCompatActivity {
+    private ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_selection);
+    backButton = findViewById(R.id.backbutton);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameSelection.this, MainMenuTest.class);
+                startActivity(intent);
+            }
+        });
 
 
 
