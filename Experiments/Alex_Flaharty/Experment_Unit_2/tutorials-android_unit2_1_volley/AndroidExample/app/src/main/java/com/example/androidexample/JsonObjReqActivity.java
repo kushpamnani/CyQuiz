8bfca545401f4e39cdf050f4ae77c476f11e9bbd
Alex_Flaharty <alex25@iastate.edu>
2024-02-20 13:43:00 -0600

    Pulls flash card data from postman and create a flash card on the ui

--- Experiments/Alex_Flaharty/Experment_Unit_2/tutorials-android_unit2_1_volley/AndroidExample/app/src/main/java/com/example/androidexample/JsonObjReqActivity.java
+++ Experiments/Alex_Flaharty/Experment_Unit_2/tutorials-android_unit2_1_volley/AndroidExample/app/src/main/java/com/example/androidexample/JsonObjReqActivity.java
@@ -22,27 +22,58 @@ import java.util.Map;
 public class JsonObjReqActivity extends AppCompatActivity {
 
     private Button btnJsonObjReq;
+    private Button flash_button;
     private TextView msgResponse;
     private TextView object;
+    private TextView title;
+    private TextView question;
+    private TextView answer;
+    private TextView wrong_1;
+    private TextView wrong_2;
+    private TextView wrong_3;
     private String type;
     private String name;
+    private String title_txt;
+    private String question_txt;
+    private String correct;
+    private String fail_1;
+    private String fail_2;
+    private String fail_3;
+    private int request_type;
 
     //private static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
     //private static final String URL_JSON_OBJECT = "http://date.jsontest.com/";
-    private static final String URL_JSON_OBJECT = "https://270850cd-16f4-4610-8e1b-a64f07b81113.mock.pstmn.io/Object/";
+    private static final String URL_JSON_OBJECT = "https://270850cd-16f4-4610-8e1b-a64f07b81113.mock.pstmn.io/name/";
+    private static final String URL_JSON_Card = "https://270850cd-16f4-4610-8e1b-a64f07b81113.mock.pstmn.io/card/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json_obj_req);
 
         btnJsonObjReq = findViewById(R.id.btnJsonObj);
+        flash_button = findViewById(R.id.card_button);
+        title = findViewById(R.id.card_title);
+        question = findViewById(R.id.question);
+        answer = findViewById(R.id.answer);
+        wrong_1 = findViewById(R.id.wrong_1);
+        wrong_2 = findViewById(R.id.wrong_2);
+        wrong_3 = findViewById(R.id.wrong_3);
         msgResponse = findViewById(R.id.Type);
         object = findViewById(R.id.Object);
 
         btnJsonObjReq.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
-                makeJsonObjReq();
+                request_type =0;
+                makeJsonObjReq(URL_JSON_OBJECT);
+            }
+        });
+        flash_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request_type =1;
+                makeJsonObjReq(URL_JSON_Card);
             }
         });
     }
@@ -50,18 +81,28 @@ public class JsonObjReqActivity extends AppCompatActivity {
     /**
      * Making json object request
      */
-    private void makeJsonObjReq() {
+    private void makeJsonObjReq(String url) {
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(
                 Request.Method.GET,
-                URL_JSON_OBJECT,
+                url,
                 null, // Pass null as the request body since it's a GET request
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d("Volley Response", response.toString());
                         JSONParse(response.toString());
-                        msgResponse.setText(type);
-                        object.setText(name);
+                        if(request_type==0){
+                            msgResponse.setText(type);
+                            object.setText(name);
+                        }
+                        else{
+                            title.setText(title_txt);
+                            question.setText(question_txt);
+                            answer.setText(correct);
+                            wrong_1.setText(fail_1);
+                            wrong_2.setText(fail_2);
+                            wrong_3.setText(fail_3);
+                        }
                     }
                 },
                 new Response.ErrorListener() {
@@ -93,6 +134,12 @@ public class JsonObjReqActivity extends AppCompatActivity {
     private void JSONParse(String text){
         type = "";
         name = "";
+        title_txt="";
+        question_txt="";
+        correct="";
+        fail_1="";
+        fail_2="";
+        fail_3="";
         int z = 0;
         int i = 0;
         int x =text.length();
@@ -101,11 +148,33 @@ public class JsonObjReqActivity extends AppCompatActivity {
                 z++;
             }
             else{
-                if(z==1){
-                    type =type + text.charAt(i);
+                if(request_type==0){
+                    if(z==1){
+                        type =type + text.charAt(i);
+                    }
+                    if(z==3){
+                        name = name + text.charAt(i);
+                    }
                 }
-                if(z==3){
-                    name = name + text.charAt(i);
+                else{
+                    if(z==3){
+                        title_txt =title_txt + text.charAt(i);
+                    }
+                    if(z==7){
+                        question_txt = question_txt + text.charAt(i);
+                    }
+                    if(z==11){
+                        correct =correct + text.charAt(i);
+                    }
+                    if(z==15){
+                        fail_1 = fail_1 + text.charAt(i);
+                    }
+                    if(z==19){
+                        fail_2 =fail_2 + text.charAt(i);
+                    }
+                    if(z==23){
+                        fail_3 = fail_3 + text.charAt(i);
+                    }
                 }
             }
             i++;
