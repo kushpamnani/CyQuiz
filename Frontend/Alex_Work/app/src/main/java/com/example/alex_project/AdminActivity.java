035b11b1b454e3b68fa30a0e2a775c2264ba693e
Alex_Flaharty <alex25@iastate.edu>
2024-02-27 15:54:00 -0600

    Finished Admin Ui

--- Frontend/Alex_Work/app/src/main/java/com/example/alex_project/AdminActivity.java
+++ Frontend/Alex_Work/app/src/main/java/com/example/alex_project/AdminActivity.java
@@ -2,16 +2,32 @@ package com.example.alex_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 
 public class AdminActivity extends AppCompatActivity {
-    private TextView test;
+    private TextView Title;
+    private TextView User_name;
+    private TextView Banned_list;
+    private Button Ban;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        Title = findViewById(R.id.Title);
+        User_name = findViewById(R.id.Ban);
+        Banned_list = findViewById(R.id.Banned_List);
+        Ban = findViewById(R.id.Ban_button);
+        Ban.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo
+                // Add Username to ban list and refresh banned list
+            }
+        });
 
     }
 }
