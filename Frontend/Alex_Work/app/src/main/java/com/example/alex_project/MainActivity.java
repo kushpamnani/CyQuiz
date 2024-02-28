4ea56107a9647aa8c8b1599799d51e9b1c588d01
Alex_Flaharty <alex25@iastate.edu>
2024-02-28 15:35:49 -0600

    Pulls from the server and edits the flash card

--- Frontend/Alex_Work/app/src/main/java/com/example/alex_project/MainActivity.java
+++ Frontend/Alex_Work/app/src/main/java/com/example/alex_project/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.alex_project;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private TextView answer;
@@ -21,11 +33,13 @@ public class MainActivity extends AppCompatActivity {
     private Button Back;
     private Button Save;
     private Button Next;
+    private String SaveUrl;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SaveUrl = "https://1b8a5bc2-eeac-4f16-a22c-dbcde8bfecdd.mock.pstmn.io/FlashCard";
         answer = findViewById(R.id.Correct);
         Wrong_1 = findViewById(R.id.Wrong_1);
         Wrong_2 = findViewById(R.id.Wrong_2);
@@ -38,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         Save = findViewById(R.id.Save);
         Card_Number_int = 1;
         Card_Number.setText("1");
-        //todo pull data from server
+        makeJsonObjReq(SaveUrl);
         Next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,5 +83,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private void Json_Parse(JSONObject response){
+        int x = 0;
+        int i = 0;
+        int slot =0; //keeps track where to put the info
+        String text = response.toString();
+        String parsed="";
+        while (x< text.length()-1){
+            if(text.charAt(x)=='\"'){
+                i++;
+            }
+            else if(i==3){
+                parsed =parsed + text.charAt(x);
+            }
+            if(i==4){
+                if(slot==0){
+                    Card_Number.setText(parsed);
+                }
+                if(slot==1){
+                    Question.setText(parsed);
+                }
+                if(slot==2){
+                    answer.setText(parsed);
+                }
+                if(slot==3){
+                    Wrong_1.setText(parsed);
+                }
+                if(slot==4){
+                    Wrong_2.setText(parsed);
+                }
+                if(slot==5){
+                    Wrong_3.setText(parsed);
+                }
+                slot++;
+                i=0;
+                parsed="";
+            }
+            x++;
+        }
+    }
+    private void makeJsonObjReq(String url) {
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Volley Response", response.toString());
+                        Json_Parse(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
     }
 }
