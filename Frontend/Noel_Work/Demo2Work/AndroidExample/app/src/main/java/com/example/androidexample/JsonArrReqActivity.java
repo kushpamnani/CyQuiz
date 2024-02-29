57610ff0598fe4b59acb3a860ccaad5e2e3ba509
Noel Binns <noelb@iastate.edu>
2024-02-28 23:00:22 -0600

    Working post, moved to correct folder

--- Frontend/Noel_Work/Demo2Work/AndroidExample/app/src/main/java/com/example/androidexample/JsonArrReqActivity.java
+++ Frontend/Noel_Work/Demo2Work/AndroidExample/app/src/main/java/com/example/androidexample/JsonArrReqActivity.java
@@ -8,65 +8,51 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class JsonObjReqActivity extends AppCompatActivity {
+public class JsonArrReqActivity extends AppCompatActivity {
 
-    private Button btnJsonObjReq;
+    private Button btnJsonArrReq;
     private TextView msgResponse;
 
-    private JSONObject TestLog;
-
-    private static final String URL_JSON_OBJECT = "https://87c2ba07-6f71-4b03-adcb-747b9cb5798c.mock.pstmn.io/PostTester/";
+    private static final String URL_JSON_ARRAY = "https://87c2ba07-6f71-4b03-adcb-747b9cb5798c.mock.pstmn.io/PostTester/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_json_obj_req);
+        setContentView(R.layout.activity_json_arr_req);
 
-        btnJsonObjReq = findViewById(R.id.btnJsonObj);
+        btnJsonArrReq = findViewById(R.id.btnJsonArr);
         msgResponse = findViewById(R.id.msgResponse);
 
-        btnJsonObjReq.setOnClickListener(new View.OnClickListener() {
+        btnJsonArrReq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                try{
-                   TestLog = new JSONObject();
-                    TestLog.put("Username", "NoelBinns");
-                    TestLog.put("Password", "password");
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-                makeJsonObjReq();
+                makeJsonArrayReq();
             }
         });
     }
 
     /**
-     * Making json object request
-     */
-    private void makeJsonObjReq() {
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
-
+     * Making json array request
+     * */
+    private void makeJsonArrayReq() {
+        JsonArrayRequest jsonArrReq = new JsonArrayRequest(
                 Request.Method.POST,
-                URL_JSON_OBJECT,
-                TestLog,
-                 // Pass null as the request body since it's a GET request
-                new Response.Listener<JSONObject>() {
+                URL_JSON_ARRAY,
+                null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONArray response) {
                         Log.d("Volley Response", response.toString());
                         msgResponse.setText(response.toString());
                     }
@@ -76,11 +62,10 @@ public class JsonObjReqActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         Log.e("Volley Error", error.toString());
                     }
-                }
-        ) {
+        }) {
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> headers = new HashMap<String, String>();
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
 //                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
 //                headers.put("Content-Type", "application/json");
                 return headers;
@@ -88,7 +73,7 @@ public class JsonObjReqActivity extends AppCompatActivity {
 
             @Override
             protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
+                Map<String, String> params = new HashMap<>();
 //                params.put("param1", "value1");
 //                params.put("param2", "value2");
                 return params;
@@ -96,6 +81,6 @@ public class JsonObjReqActivity extends AppCompatActivity {
         };
 
         // Adding request to request queue
-        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrReq);
     }
 }
