7dfb860d0734d6e6aa01c866607cb7be333376a3
Noel Binns <noelb@iastate.edu>
2024-03-01 15:33:14 -0600

    classroom screen WIP

--- Experiments/Noel_Binns/MenuExperiment/AndroidExample/app/src/main/java/com/example/androidexample/ClassroomMain.java
+++ Experiments/Noel_Binns/MenuExperiment/AndroidExample/app/src/main/java/com/example/androidexample/ClassroomMain.java
@@ -1,2 +1,16 @@
-package com.example.androidexample;public class ClassroomMain {
+package com.example.androidexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClassroomMain extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.classroom_main);
+
+
+
+    }
 }
