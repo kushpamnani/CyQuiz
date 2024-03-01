b7a57f2654409092c18cd0e3d4e5f4a2a5a2db53
Noel Binns <noelb@iastate.edu>
2024-03-01 01:39:00 -0600

    working demo2 code, needs slight more autonomy

--- Frontend/Noel_Work/Demo2Work/AndroidExample/app/src/main/java/com/example/androidexample/JsonObjReqActivity.java
+++ Frontend/Noel_Work/Demo2Work/AndroidExample/app/src/main/java/com/example/androidexample/JsonObjReqActivity.java
@@ -28,8 +28,12 @@ public class JsonObjReqActivity extends AppCompatActivity {
 
     private JSONObject TestLog;
 
-    private static final String URL_JSON_OBJECT = "https://87c2ba07-6f71-4b03-adcb-747b9cb5798c.mock.pstmn.io/PostTester/";
+    private TextView testText;
 
+    private static final String URL_JSON_OBJECT = "https://87c2ba07-6f71-4b03-adcb-747b9cb5798c.mock.pstmn.io/PostTester/";
+   // private static final String URL_JSON_OBJECT = "http://coms-309-031.class.las.iastate.edu:8080/users";
+    //private static final String URL_JSON_OBJECT = "http://coms-309-031.class.las.iastate.edu:8080/teachers";
+   // private static final String URL_JSON_OBJECT = "http://coms-309-031.class.las.iastate.edu:8080/teachers/9/add_classroom";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +41,7 @@ public class JsonObjReqActivity extends AppCompatActivity {
 
         btnJsonObjReq = findViewById(R.id.btnJsonObj);
         msgResponse = findViewById(R.id.msgResponse);
+        testText = findViewById(R.id.textView3);
 
         btnJsonObjReq.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +49,8 @@ public class JsonObjReqActivity extends AppCompatActivity {
 
                 try{
                    TestLog = new JSONObject();
-                    TestLog.put("Username", "NoelBinns");
-                    TestLog.put("Password", "password");
+                    TestLog.put("name", "Test Classroom");
+                    //TestLog.put("password", "password");
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
@@ -69,6 +74,28 @@ public class JsonObjReqActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         Log.d("Volley Response", response.toString());
                         msgResponse.setText(response.toString());
+//                        String HelperString = response.toString();
+//                        int length = HelperString.length();
+//
+//                        String Answer = "";
+//                        int counter = 0;
+//                        for (int i = 0; i < length; i++){
+//                            if (HelperString.charAt(i) == '\"') {
+//                                counter++;
+//                            }
+//                            else if (counter == 3) {
+//
+//
+//                                Answer = Answer + HelperString.charAt(i);
+//
+//
+//                            }
+//                            if (counter == 4){
+//                                counter = 0;
+//                            }
+//                        }
+//
+//                        testText.setText(Answer);
                     }
                 },
                 new Response.ErrorListener() {
--- Frontend/Noel_Work/Demo2Work/AndroidExample/app/src/main/java/com/example/androidexample/StringReqActivity.java
+++ Frontend/Noel_Work/Demo2Work/AndroidExample/app/src/main/java/com/example/androidexample/StringReqActivity.java
@@ -1,5 +1,9 @@
 package com.example.androidexample;
 
+import static android.text.TextUtils.indexOf;
+import static android.text.TextUtils.lastIndexOf;
+import static android.text.TextUtils.substring;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -14,6 +18,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,8 +28,13 @@ public class StringReqActivity extends AppCompatActivity {
     private Button btnStringReq;
     private TextView msgResponse;
 
+    private TextView TestText;
+
+
+
+
    private static final String URL_STRING_REQ = "https://4438904a-d327-4dc4-a0d4-57eb49e95091.mock.pstmn.io/work_example/";
-      // public static final String URL_STRING_REQ = "https://2aa87adf-ff7c-45c8-89bc-f3fbfaa16d15.mock.pstmn.io/users/1";
+     // public static final String URL_STRING_REQ = "http://coms-309-031.class.las.iastate.edu:8080/classrooms/23";
     //   public static final String URL_STRING_REQ = "http://10.0.2.2:8080/users/1";
 
     @Override
@@ -31,8 +42,10 @@ public class StringReqActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_string_req);
 
+
         btnStringReq = (Button) findViewById(R.id.btnStringReq);
         msgResponse = (TextView) findViewById(R.id.msgResponse);
+        TestText = (TextView) findViewById(R.id.textView2);
 
         btnStringReq.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,11 +67,35 @@ public class StringReqActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Handle the successful response here
-                        Log.d("Volley Response", response);
-                        msgResponse.setText(response.toString());
+                        Log.d("Volley Response", response.toString());
+                        //msgResponse.setText(response.toString());
+
+                         String HelperString = response.toString();
+                         int length = HelperString.length();
+
+                        String Answer = "";
+                        int counter = 0;
+                         for (int i = 0; i < length; i++){
+                             if (HelperString.charAt(i) == '\"') {
+                                counter++;
+                             }
+                               else if (counter == 3) {
+
+
+                                       Answer = Answer + HelperString.charAt(i);
+
+
+                               }
+                             if (counter == 4){
+                                 counter = 0;
+                             }
+                         }
+
+                        TestText.setText(Answer);
+
                     }
                 },
+
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
