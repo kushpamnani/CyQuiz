5ad8b09f8a310647cfb27d73f89487eefb4908f2
Noel Binns <noelb@iastate.edu>
2024-03-31 12:44:36 -0500

    enemy creator start

---
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/EnemyCreatorActivity.java
@@ -0,0 +1,19 @@
+package com.example.androidexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnemyCreatorActivity extends AppCompatActivity {
+    private static final String URL_JSON_OBJECT = "http://coms-309-031.class.las.iastate.edu:8080";
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enemy_creator);
+
+
+    }
+
+}
