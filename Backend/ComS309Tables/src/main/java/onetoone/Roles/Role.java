b8dec79f04f0d54dfb0e125afc319924a1b260ef
Kush Pamnani <kushpamnani@Kushs-MacBook-Air.local>
2024-04-03 13:43:10 -0500

    Refactor backend structure and update documentation

---
+++ Backend/ComS309Tables/src/main/java/onetoone/Roles/Role.java
@@ -0,0 +1,64 @@
+package onetoone.Roles;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//import onetoone.Users.User;
+
+/**
+ *
+ * @author Vivek Bengre
+ */
+
+@Entity
+public class Role {
+
+    /*
+     * The annotation @ID marks the field below as the primary key for the table created by springboot
+     * The @GeneratedValue generates a value if not already present, The strategy in this case is to start from 1 and increment for each table
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String roleName;
+
+    /*
+     * @OneToOne creates a relation between the current entity/table(Laptop) with the entity/table defined below it(User)
+     * @JsonIgnore is to assure that there is no infinite loop while returning either user/laptop objects (laptop->user->laptop->...)
+     */
+//    @OneToOne
+//    @JsonIgnore
+//    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Role(int id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Role() {
+    }
+
+
+
+}
