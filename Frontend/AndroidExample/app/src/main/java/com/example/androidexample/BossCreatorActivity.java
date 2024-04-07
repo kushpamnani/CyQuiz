e61ca777bfa853ef7485d1bd7bb13a6885f29761
Noel Binns <noelb@iastate.edu>
2024-04-06 23:01:07 -0500

    battle and boss creators mostly done

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/BossCreatorActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/BossCreatorActivity.java
@@ -9,12 +9,16 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +29,11 @@ public class BossCreatorActivity extends AppCompatActivity {
     private TextView dmgGet;
     private TextView flashGet;
     private TextView errorText;
+    private TextView defenseGet;
+    private TextView nameGet;
     private Button sendInfo;
+    private JSONObject bossJSON;
+    private boolean flashcardExist = true;
 
     private String baseURL = "http://coms-309-031.class.las.iastate.edu:8080";
 
@@ -39,21 +47,33 @@ public class BossCreatorActivity extends AppCompatActivity {
         flashGet = findViewById(R.id.cardInput);
         sendInfo = findViewById(R.id.sendBtn);
         errorText = findViewById(R.id.errorText);
+        defenseGet = findViewById(R.id.defenseInput);
+        nameGet = findViewById(R.id.bossNameInput);
 
 
         sendInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                bossJSON = new JSONObject();
+                try {
+                    bossJSON.put("name", nameGet.getText().toString());
+                    bossJSON.put("health", healthGet.getText().toString());
+                    bossJSON.put("damage", dmgGet.getText().toString());
+                    bossJSON.put("defense", defenseGet.getText().toString());
+                    bossJSON.put("flashcard", flashGet.getText().toString());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
                 makeJsonArrayReq();
 
+                if (flashcardExist) {
+                    makeJsonObjReq();
+                }
 
 
             }
         });
-
-
-
-
 
 
     }
@@ -67,7 +87,28 @@ public class BossCreatorActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONArray response) {
                         Log.d("Volley Response", response.toString());
-                        errorText.setText(response.toString());
+
+                        for (int i = 0; i <= response.length(); i++) {
+                            if (response.optJSONObject(i).optString("id").equals(flashGet.getText().toString())) {
+                                errorText.setText(response.optJSONObject(i).toString());
+                                flashcardExist = true;
+                                break;
+                            }
+                        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     }
                 },
                 new Response.ErrorListener() {
@@ -95,6 +136,50 @@ public class BossCreatorActivity extends AppCompatActivity {
 
         // Adding request to request queue
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrReq);
+    }
+
+    private void makeJsonObjReq() {
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+
+                Request.Method.POST,
+                baseURL + "/enemies",
+                bossJSON,
+                // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Volley Response", response.toString());
+                        errorText.setText(response.toString());
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
     }
 
 }
