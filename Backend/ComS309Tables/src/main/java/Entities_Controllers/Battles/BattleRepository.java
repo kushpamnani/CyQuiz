ab66b2b8067c4458d4ab5eb30c632d34a5da660f
Kush Pamnani <137836006+kushpamnani@users.noreply.github.com>
2024-05-22 17:46:58 -0400

    Update BattleRepository.java

--- Backend/ComS309Tables/src/main/java/Entities_Controllers/Battles/BattleRepository.java
+++ Backend/ComS309Tables/src/main/java/Entities_Controllers/Battles/BattleRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 /**
  *
- * @author Dalton Clark
+ * @author Kush Pamnani
  *
  */
 public interface BattleRepository extends JpaRepository<Battle, Long> {
