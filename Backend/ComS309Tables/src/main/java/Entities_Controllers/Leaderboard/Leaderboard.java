9452129d4f4f57676a6af0632227fdc1d2bf162d
Kush Pamnani <137836006+kushpamnani@users.noreply.github.com>
2024-05-23 20:21:32 -0400

    Update Leaderboard.java

--- Backend/ComS309Tables/src/main/java/Entities_Controllers/Leaderboard/Leaderboard.java
+++ Backend/ComS309Tables/src/main/java/Entities_Controllers/Leaderboard/Leaderboard.java
@@ -5,6 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+/**
+ * 
+ * @author Kush Pamnani
+ * 
+ */
+
 @Entity
 public class Leaderboard {
 
