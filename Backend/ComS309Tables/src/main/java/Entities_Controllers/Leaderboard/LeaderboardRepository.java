e5b21007ecec0d70616e7d877aa9dec688a730d8
Kush Pamnani <137836006+kushpamnani@users.noreply.github.com>
2024-05-23 20:22:04 -0400

    Update LeaderboardRepository.java

--- Backend/ComS309Tables/src/main/java/Entities_Controllers/Leaderboard/LeaderboardRepository.java
+++ Backend/ComS309Tables/src/main/java/Entities_Controllers/Leaderboard/LeaderboardRepository.java
@@ -3,7 +3,12 @@ package Entities_Controllers.Leaderboard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 
+ * @author Kush Pamnani
+ * 
+ */
+
 @Repository
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
-    // Additional custom methods can be declared here if necessary
 }
