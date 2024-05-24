4cf5578cf32877a188a56747d9ea1256e2501a5b
Kush Pamnani <kushpamnani@Kushs-MacBook-Air.local>
2024-05-23 20:33:32 -0400

    cleaning

--- Documents/NoelJavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.androidexample","l":"ImageReqActivity"},{"p":"com.example.androidexample","l":"JsonObjReqActivity"},{"p":"com.example.androidexample","l":"StringReqActivity"}];updateSearchResults();
